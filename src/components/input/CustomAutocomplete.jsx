50fb86ba965e9be89abccade692c3984f7417b7d
asifazad799 <asifazad799@gmail.com>
2024-05-05 16:23:45 +0400

    add - custom input

--- src/components/input/CustomAutocomplete.jsx
+++ src/components/input/CustomAutocomplete.jsx
@@ -2,28 +2,28 @@ import * as React from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 
-export function CustomAutocomplete() {
+export function CustomAutocomplete({ options, ...props }) {
   return (
     <Autocomplete
       multiple
       limitTags={2}
-      id="multiple-limit-tags"
-      options={top100Films}
-      getOptionLabel={(option) => option.title}
-      defaultValue={[top100Films[13], top100Films[12], top100Films[11]]}
-      renderInput={(params) => (
-        <TextField {...params} label="limitTags" placeholder="Favorites" />
-      )}
-      sx={{ width: "500px" }}
+      options={options || top100Films}
+      // groupBy={(option) => option.year}
+      renderInput={(params) => <TextField {...params} label="Autocomplete" />}
+      getOptionLabel={(option) => option?.title}
+      ListboxProps={{
+        "aria-labelledby": "nested-list-subheader",
+      }}
+      {...props}
     />
   );
 }
 
 const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+  },
   { title: "12 Angry Men", year: 1957 },
   { title: "Schindler's List", year: 1993 },
   { title: "Pulp Fiction", year: 1994 },
